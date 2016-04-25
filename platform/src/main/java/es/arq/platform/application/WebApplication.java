2b0c19d0c93e13d53d2f35a4ed2b6374b24b073b
Miguel Moran Cassy <miguelmorancassy@gexterno.es>
2016-04-25 10:04:57 +0200

    Añade conexión segura https

--- platform/src/main/java/es/arq/platform/application/WebApplication.java
+++ platform/src/main/java/es/arq/platform/application/WebApplication.java
@@ -1,8 +1,15 @@
 package es.arq.platform.application;
 
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.jsondoc.spring.boot.starter.EnableJSONDoc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
@@ -15,5 +22,36 @@ public class WebApplication {
     public static void main(String[] args) throws Exception {
         SpringApplication.run(WebApplication.class, args);
     }
+    
+    @Bean
+    public EmbeddedServletContainerFactory servletContainer() {
+      TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory() {
+          @Override
+          protected void postProcessContext(Context context) {
+            SecurityConstraint securityConstraint = new SecurityConstraint();
+            securityConstraint.setUserConstraint("CONFIDENTIAL");
+            SecurityCollection collection = new SecurityCollection();
+            collection.addPattern("/*");
+            securityConstraint.addCollection(collection);
+            context.addConstraint(securityConstraint);
+          }
+        };
+      
+      tomcat.addAdditionalTomcatConnectors(initiateHttpConnector());
+      return tomcat;
+    }
+    
+    /*
+     * Redirect http connection to https
+     */
+    private Connector initiateHttpConnector() {
+      Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+      connector.setScheme("http");
+      connector.setPort(8080);
+      connector.setSecure(false);
+      connector.setRedirectPort(8443);
+      
+      return connector;
+    }    
 
 }
